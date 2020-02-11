63fa9b0f345c30f6b052db1c295f98cc76eddda4
NW <nathan@nnorris.com>
2020-02-11 10:06:13 -0600

    main file system init

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # RMD_AppServer
+npm run run
---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+var fs = require('fs');
+var cors = require('cors');
+var cookie = require('cookie-parser');
+const port = 82;
+
+//IO config
+//EJS config
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+//Json and directory config
+app.use(express.json());
+app.use('/js', express.static(__dirname + '/src/js/'));
+app.use('/css', express.static(__dirname + '/src/css/'));
+app.use('/font', express.static(__dirname + '/src/font/'));
+app.use('/img', express.static(__dirname + '/src/img/'));
+
+//Other config
+app.use(cors())
+app.use(cookie())
+
+const server = app.listen(port, () => console.log('[STARTUP] RDM_AppServer online on port ' + port))
