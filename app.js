21681c4481fac161960593c3d6bdfa1a8c8c871f
jb-tomaszewski <tomaszewskijimmy@gmail.com>
2020-02-18 11:22:47 -0600

    Added more dad skills

--- app.js
+++ app.js
@@ -32,19 +32,38 @@ const server = app.listen(port, () => console.log('[STARTUP] RDM_AppServer onlin
 ///---DAD_PROFILE:CREATE (POST)---///
 router.post("/dad_profile/create", function(req, res, next) {
   var params = req.body.params;
-  var name = params.name
+  var name = params.name;
+  var skills = params.skills; 
 
   if(name.first != undefined && name.last != undefined) {
     console.log("recieved request to create: " + name.first + " " + name.last);
   }
 
+  console.log(skills); 
+
   var dad = new DadProfile({
-    name: {first: name.first, last: name.last}, skills : {grill: 3, bags: 3}
+    name: {first: name.first, last: name.last}, 
+    skills : {grilling: skills.grilling, 
+              cooking: skills.cooking, 
+              bags: skills.bags, 
+              softball: skills.softball,
+              coaching: skills.coaching,
+              generosity: skills.generosity,
+              looks: skills.looks, 
+              dad_factor: skills.dad_factor, 
+              fantasy_football: skills.fantasy_football,
+              humor: skills.humor,
+              emotional_stability: skills.emotional_stability, 
+              handiness: skills.handiness, 
+              kids: skills.kids, 
+              stealth_food_preparation: skills.stealth_food_preparation,
+              tech: skills.tech, 
+              furniture_assembly: skills.furniture_assembly, 
+              photography: skills.photography}
   });
 
   dad.save()
   res.send(dad)
-
 })
 
 ///---INDEX (POST)---///
