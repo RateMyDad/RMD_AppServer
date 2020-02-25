447ce9507de6dbeb47fe99827249c38cecc5950d
jb-tomaszewski <tomaszewskijimmy@gmail.com>
2020-02-24 22:57:57 -0600

    (Somewhat) implemented dad rankings based on skill score

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 var router = express.Router();
 var cors = require('cors');
 var bcrypt = require('bcrypt');
+var session = require('express-session');
 const port = 82;
 
 //---MONGOOSE & MONGODB SETUP---//
@@ -14,12 +15,13 @@ db.once('open', function(){
   console.log("established DB connection.");
 });
 
+//---Load Mongoose Models---//
 const DadProfile = require('./models/DadProfile');
 const User = require('./models/User');
 
 //---Other misc config---//
 app.use(express.json());
-
+app.use(session({ secret: 'peepeepoopoomommygrandma', cookie: { maxAge: 60000}, username: undefined}));
 //---Enable routing for configured endpoints---///
 app.use("/", router);
 app.use("/dad_profile/create", router);
@@ -29,41 +31,147 @@ app.use("/user/login", router);
 //Run server
 const server = app.listen(port, () => console.log('[STARTUP] RDM_AppServer online on port ' + port))
 
+///---HELPER FUNCTIONS---///
 async function user_exists(username) {
   var user = await User.findOne({"username": username}).exec();
   console.log("[user_exists] " + username + ": " + (user != null))
   return (user != null)
 }
 
+// GET DAD RATING 
+function getRatings() {
+  DadProfile.find({}).sort('meta.skillScore').exec(function(err, docs) {
+    if (!err) {
+      console.log(docs); 
+      var count = 1; 
+      for (var i = docs.length - 1; i >= 0; i--) {
+        docs[i].meta.rating = count; 
+        docs[i].save(); 
+        count++; 
+      }
+
+      console.log("--------------------------------------------");
+      console.log(docs); 
+    }
+
+    else {
+      console.log(err); 
+    }
+  })
+}
+
+async function getSkillScore(skills) {
+  var total = 0
+
+  total = total + (skills.grilling * 1.5)
+  + (skills.cooking * 2)
+  + (skills.bags)
+  + (skills.softball)
+  + (skills.coaching * 1.5)
+  + (skills.generosity * 3)
+  + (skills.looks * 1.5)
+  + (skills.dad_factor * 2.5)
+  + (skills.fantasy_football)
+  + (skills.humor * 2.5)
+  + (skills.emotional_stability * 2)
+  + (skills.handiness * 3)
+  + (skills.kids * 3)
+  + (skills.stealth_food_preparation)
+  + (skills.tech * 2)
+  + (skills.furniture_assembly)
+  + (skills.photography)
+
+  return total
+}
+
 ///---DAD_PROFILE:CREATE (POST)---///
 
 
-  console.log(skills); 
+router.post("/dad_profile/create", async function(req, res, next) {
 
-  var dad = new DadProfile({
-    name: {first: name.first, last: name.last}, 
-    skills : {grilling: skills.grilling * 1.5, 
-              cooking: skills.cooking * 2, 
-              bags: skills.bags, 
-              softball: skills.softball,
-              coaching: skills.coaching * 1.5,
-              generosity: skills.generosity * 3,
-              looks: skills.looks * 1.5, 
-              dad_factor: skills.dad_factor * 2.5, 
-              fantasy_football: skills.fantasy_football,
-              humor: skills.humor * 2.5,
-              emotional_stability: skills.emotional_stability * 2, 
-              handiness: skills.handiness * 3, 
-              kids: skills.kids * 3, 
-              stealth_food_preparation: skills.stealth_food_preparation,
-              tech: skills.tech * 1.5, 
-              furniture_assembly: skills.furniture_assembly, 
-              photography: skills.photography}
-  });
+  if(req.session.username == undefined){
 
-  dad.save()
-  res.send(dad)
-})
+    res.status(400).send({message: "You must be logged in to create a profile."});
+
+  } else {
+
+    User.findOne({"username": req.session.username}).exec(async function(err, result) {
+
+        if(result.profile.parent_profile != null) {
+          res.status(400).send({message: "You already have a profile created!"});
+          return false;
+        }
+        //Test comment
+        var exists = await user_exists(req.body.username);
+        if(exists) {
+
+          if(req.body.name.first != undefined && req.body.name.last != undefined) {
+
+            console.log("[dad_profile/create] Profile creation for : " + req.body.name.first + " " + req.body.name.last)
+            var skills = req.body.skills;
+
+            var skillScore = await getSkillScore(skills); 
+
+            var dad = new DadProfile({
+              name: {
+                first: req.body.name.first,
+                last: req.body.name.last
+              },
+
+              skills : {
+                grilling: skills.grilling,
+                cooking: skills.cooking,
+                bags: skills.bags,
+                softball: skills.softball,
+                coaching: skills.coaching,
+                generosity: skills.generosity,
+                looks: skills.looks,
+                dad_factor: skills.dad_factor,
+                fantasy_football: skills.fantasy_football,
+                humor: skills.humor,
+                emotional_stability: skills.emotional_stability,
+                handiness: skills.handiness,
+                kids: skills.kids,
+                stealth_food_preparation: skills.stealth_food_preparation,
+                tech: skills.tech,
+                furniture_assembly: skills.furniture_assembly,
+                photography: skills.photography
+              },
+
+              location : {
+                country: "United States",
+                region: "US-Central"
+              },
+
+              meta: {
+                rating: 0,
+                skillScore: skillScore
+              }
+
+           });
+
+            console.log("[dad_profile/create] Linking profile {" + req.body.name.first + " " + req.body.name.last + "} to user " + req.body.username);
+            var user_toLink = await User.findOne({"username": req.body.username}).exec();
+            user_toLink.profile.parent_profile = dad._id;
+
+            dad.save();
+            user_toLink.save();
+
+            res.send(dad)
+
+          } else {
+            res.status(400).send({message: "Missing first and last name."});
+          }
+
+        } else {
+          res.status(400).send({message: "User not found."});
+        }
+    });
+
+    // Updates ratings for all dads, although not including the dad being posted (for some reason).  
+    getRatings(); 
+  }
+});
 
 ///---INDEX (POST)---///
 //Currently for testing, provide a dad first name in body.params and it returns a matching dad from DB
@@ -77,7 +185,6 @@ router.post("/", function(req, res, next) {
     res.send(docs)
   })
 
-  //res.render("index");
 });
 
 ///---USER:REGISTER (POST)---///
@@ -86,6 +193,7 @@ router.post("/user/register", async function(req, res, next) {
   try {
 
     console.log("/register: " + req.body.username);
+    console.log("password:" + req.body.password); 
       var user = User.findOne({ "username": req.body.username }).exec(function(err, result) {
         if(result == undefined) {
           req.body.password = bcrypt.hashSync(req.body.password, 10);
@@ -112,29 +220,44 @@ router.post("/user/register", async function(req, res, next) {
 
 });
 
+///---USER:LOGOUT (GET)---///
+router.get("/user/logout", async function(req, res, next) {
+  if(req.session.username == undefined) {
+    res.status(400).send({message: "You are not logged in!"});
+  } else {
+    console.log("Username: " + req.session.username); 
+    var user = req.session.username;
+    req.session.destroy();
+    res.status(200).send({messsage: "Sucessfully logged out " + user});
+  }
+})
+
 ///---USER:LOGIN (POST)---///
 router.post("/user/login", async function(req, res, next) {
-  
-  try {
-    console.log("/login " + req.body.username)
-    var user = await User.findOne({ "username": req.body.username }).exec()
+  if(req.session.username != undefined) {
+    res.status(200).send({message: "You are already logged in as " + req.session.username});
+  } else {
+    try {
+      console.log("/login " + req.body.username)
+      var user = await User.findOne({ "username": req.body.username }).exec()
 
-    if(!user) {
-      //User does not exist
-      return res.status(400).send({message: "User not found"});
+      if(!user) {
+        //User does not exist
+        return res.status(400).send({message: "User not found"});
+      }
+
+      if(!bcrypt.compareSync(req.body.password, user.password)) {
+        //Bad password
+        return res.status(400).send({message: "Invalid login"});
+      }
+
+      //If you get here, successful login
+      req.session.username = req.body.username;
+      return res.status(200).send(user);
+
+    } catch (error) {
+      console.log(error);
+      res.status(500).send(error);
     }
-
-    if(!bcrypt.compareSync(req.body.password, user.password)) {
-      //Bad password
-      return res.status(400).send({message: "Invalid login"});
-    }
-
-    //If you get here, successful login
-    return res.status(200).send(user);
-
-  } catch (error) {
-    console.log(error);
-    res.status(500).send(error);
-
   }
 })
--- models/DadProfile.js
+++ models/DadProfile.js
@@ -9,23 +9,23 @@ const DadProfileSchema = new mongoose.Schema({
 
   skills: {
     grilling: Number,
-    cooking: Number, 
-    bags: Number, 
-    golf: Number, 
-    softball: Number, 
-    coaching: Number, 
-    generosity: Number, 
-    looks: Number, 
-    dad_factor: Number, 
-    fantasy_football: Number, 
-    humor: Number, 
-    emotional_stability: Number, 
-    handiness: Number, 
-    kids: Number, 
-    stealth_food_preparation: Number, 
-    tech: Number, 
-    furniture_assembly: Number, 
-    photography: Number 
+    cooking: Number,
+    bags: Number,
+    golf: Number,
+    softball: Number,
+    coaching: Number,
+    generosity: Number,
+    looks: Number,
+    dad_factor: Number,
+    fantasy_football: Number,
+    humor: Number,
+    emotional_stability: Number,
+    handiness: Number,
+    kids: Number,
+    stealth_food_preparation: Number,
+    tech: Number,
+    furniture_assembly: Number,
+    photography: Number
   },
 
   location: {
@@ -34,10 +34,10 @@ const DadProfileSchema = new mongoose.Schema({
   },
 
   meta: {
-    rating: Number
+    rating: Number,
+    skillScore: Number
   }
 
 });
-
 const DadProfile = mongoose.model('DadProfile', DadProfileSchema);
 module.exports = DadProfile;
