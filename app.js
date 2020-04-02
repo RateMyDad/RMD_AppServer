3f76cbc8969e3233df37d439c222671b6043ace3
NW <nathan@nnorris.com>
2020-04-02 10:56:25 -0500

    added /dad_profile/me

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const port = 82;
 
 //---MONGOOSE & MONGODB SETUP---//
 var mongoose = require('mongoose');
-mongoose.connect("mongodb://localhost/ratemydad", {useNewUrlParser: true});
+mongoose.connect("mongodb://localhost/ratemydad", {useNewUrlParser: true, useUnifiedTopology: true});
 var db = mongoose.connection
 db.on('error', console.error.bind(console, 'db connection error:'));
 db.once('open', function(){
@@ -26,6 +26,7 @@ app.use(session({ secret: 'peepeepoopoomommygrandma', cookie: { maxAge: 60000}, 
 app.use("/", router);
 app.use("/dad_profile/create", router);
 app.use("/dad_profile/ratings", router);
+app.use("/dad_profile/me", router)
 app.use("/user/register", router);
 app.use("/user/login", router);
 app.use("/user/check_status", router);
@@ -43,21 +44,21 @@ async function user_exists(username) {
 router.get("/dad_profile/ratings", function(req, res, next) {
   DadProfile.find({}).sort('meta.skillScore').exec(function(err, docs) {
     if (!err) {
-      var count = 1; 
+      var count = 1;
       for (var i = docs.length - 1; i >= 0; i--) {
-        docs[i].meta.rating = count; 
-        docs[i].save(); 
-        count++; 
+        docs[i].meta.rating = count;
+        docs[i].save();
+        count++;
       }
 
       console.log("--------------------------------------------");
-      console.log(docs); 
+      console.log(docs);
 
-      res.status(200).send(docs); 
+      res.status(200).send(docs);
     }
 
     else {
-      console.log(err); 
+      console.log(err);
     }
   })
 });
@@ -65,23 +66,23 @@ router.get("/dad_profile/ratings", function(req, res, next) {
 /*
 =======
 <<<<<<< HEAD
-// GET DAD RATING 
+// GET DAD RATING
 >>>>>>> 721314c729fab3a260c911285432db4c38ad7e0e
 function getRatings() {
   DadProfile.find({}).sort('meta.skillScore').exec(function(err, docs) {
     if (!err) {
-      console.log(docs); 
-      var count = 1; 
+      console.log(docs);
+      var count = 1;
       for (var i = docs.length - 1; i >= 0; i--) {
-        docs[i].meta.rating = count; 
-        docs[i].save(); 
-        count++; 
+        docs[i].meta.rating = count;
+        docs[i].save();
+        count++;
       }
       console.log("--------------------------------------------");
-      console.log(docs); 
+      console.log(docs);
     }
     else {
-      console.log(err); 
+      console.log(err);
     }
   })
 }
@@ -114,10 +115,10 @@ async function getSkillScore(skills) {
 ///---DAD_PROFILE:CREATE (POST)---///
 router.post("/dad_profile/create", async function(req, res, next) {
   console.log("Info before creating dad profile:");
-  console.log(req.session); 
-  console.log(req.session.username); 
+  console.log(req.session);
+  console.log(req.session.username);
 
-  if (req.session.username == undefined){ 
+  if (req.session.username == undefined){
     res.status(400).send({message: "You must be logged in to create a profile."});
 
   } else {
@@ -129,7 +130,7 @@ router.post("/dad_profile/create", async function(req, res, next) {
           return false;
         }
         //Test comment
-        var exists = await user_exists(req.body.username);
+        var exists = await user_exists(req.session.username);
         if(exists) {
 
           if(req.body.name.first != undefined && req.body.name.last != undefined) {
@@ -139,7 +140,7 @@ router.post("/dad_profile/create", async function(req, res, next) {
 
             var zip = req.body.zip;
 
-            var skillScore = await getSkillScore(skills); 
+            var skillScore = await getSkillScore(skills);
 
             var dad = new DadProfile({
               name: {
@@ -181,18 +182,18 @@ router.post("/dad_profile/create", async function(req, res, next) {
 
            });
 
-            console.log("[dad_profile/create] Linking profile {" + req.body.name.first + " " + req.body.name.last + "} to user " + req.body.username);
-            var user_toLink = await User.findOne({"username": req.body.username}).exec();
+            console.log("[dad_profile/create] Linking profile {" + req.body.name.first + " " + req.body.name.last + "} to user " + req.session.username);
+            var user_toLink = await User.findOne({"username": req.session.username}).exec();
             user_toLink.profile.parent_profile = dad._id;
 
             console.log("B Dad saved!");
             dad.save();
-            console.log("A Dad saved!"); 
+            console.log("A Dad saved!");
             user_toLink.save();
 
             res.send(dad)
 
-            getRatings(); 
+            getRatings();
 
           } else {
             res.status(400).send({message: "Missing first and last name."});
@@ -205,18 +206,32 @@ router.post("/dad_profile/create", async function(req, res, next) {
   }
 });
 
-///---INDEX (POST)---///
-//Currently for testing, provide a dad first name in body.params and it returns a matching dad from DB
-router.post("/", function(req, res, next) {
+// ///---INDEX (POST)---///
+// //Currently for testing, provide a dad first name in body.params and it returns a matching dad from DB
+// router.post("/", function(req, res, next) {
+//
+//   var params = req.body.params
+//
+//   console.log("Params: " + params)
+//
+//   DadProfile.find({'name.first': params.name.first}, function(err, docs) {
+//     res.send(docs)
+//   })
+//
+// });
 
-  var params = req.body.params
-
-  console.log("Params: " + params)
-
-  DadProfile.find({'name.first': params.name.first}, function(err, docs) {
-    res.send(docs)
-  })
-
+router.post("/dad_profile/me", function(req, res, next) {
+  if(req.session.username == undefined) {
+    console.log(req.session)
+    res.status(400).send("You are not logged in.")
+  } else {
+    User.findOne({"username" : req.session.username}).exec(async function(err, result) {
+      var profile_id = result.profile.parent_profile;
+      console.log("[/dad_profile/me][" + req.session.username + "] profile id: " + profile_id)
+      var dad = await DadProfile.findOne({"_id" : profile_id}).exec()
+      res.status(200).send(dad)
+    });
+  }
 });
 
 ///---USER:REGISTER (POST)---///
@@ -225,7 +240,7 @@ router.post("/user/register", async function(req, res, next) {
   try {
 
     console.log("/register: " + req.body.username);
-    console.log("password:" + req.body.password); 
+    console.log("password:" + req.body.password);
       var user = User.findOne({ "username": req.body.username }).exec(function(err, result) {
         if(result == undefined) {
           req.body.password = bcrypt.hashSync(req.body.password, 10);
@@ -257,7 +272,7 @@ router.get("/user/logout", async function(req, res, next) {
   if(req.session.username == undefined) {
     res.status(400).send({message: "You are not logged in!"});
   } else {
-    console.log("Username: " + req.session.username); 
+    console.log("Username: " + req.session.username);
     var user = req.session.username;
     req.session.destroy();
     res.status(200).send({messsage: "Sucessfully logged out " + user});
@@ -267,7 +282,7 @@ router.get("/user/logout", async function(req, res, next) {
 ///---USER:CHECK_STATUS (GET)---///
 router.get("/user/check_status", async function(req, res, next) {
   console.log("Session in check_status:");
-  console.log(req.session); 
+  console.log(req.session);
   if (req.session.username == undefined) {
     res.status(400).send({message: "You must be logged in to use this feature."})
   } else {
@@ -281,7 +296,7 @@ router.get("/user/check_status", async function(req, res, next) {
       }
     });
 
-    req.session.save(); 
+    req.session.save();
   }
 })
 
@@ -308,7 +323,7 @@ router.post("/user/login", async function(req, res, next) {
       req.session.username = req.body.username;
       req.session.save(); // *** ADDING THIS TO SEE IF IT WORKS ***
       console.log("Session in login:");
-      console.log(req.session); 
+      console.log(req.session);
       return res.status(200).send(user);
 
     } catch (error) {
